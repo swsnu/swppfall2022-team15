9402a5282f1a9f8217ce61acd3eff9faca76be87
vietman2 <vietman0@gmail.com>
2022-11-05 05:41:39 +0900

    resize sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import NotiSidebar from "./Sidebar/NotiSidebar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={ { height: '100vh' } }>
       <NotiSidebar/>
     </div>
   );
--- frontend/src/Sidebar/NotiSidebar.tsx
+++ frontend/src/Sidebar/NotiSidebar.tsx
@@ -1,23 +1,43 @@
-import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
+import { Sidebar, Menu, MenuItem, useProSidebar, SidebarProps } from "react-pro-sidebar";
 import { FaHome, FaHistory, FaFolderOpen } from "react-icons/fa"
 import { FiTarget } from "react-icons/fi"
 import { HiTemplate } from "react-icons/hi"
 import { MdMessage } from "react-icons/md"
+import { AiOutlineMenuUnfold } from "react-icons/ai"
+
+import "./NotiSidebar.css";
 
 export default function NotiSidebar() {
-    //const { collapse } = useProSidebar();
+    const { collapseSidebar, collapsed } = useProSidebar();
+
+    const homeIcon = <FaHome size='48'></FaHome>
+    const projectIcon = <FaFolderOpen size='48'></FaFolderOpen>
+    const targetIcon = <FiTarget size='48'></FiTarget>
+    const messageIcon = <MdMessage size='48'></MdMessage>
+    const templateIcon = <HiTemplate size='48'></HiTemplate>
+    const historyIcon = <FaHistory size='48'></FaHistory>
+    const toggleIcon = <AiOutlineMenuUnfold size='36'></AiOutlineMenuUnfold>
 
     return (
-        <Sidebar>
-            <Menu>
-               <MenuItem icon={<FaHome />}> Home </MenuItem>
-               <MenuItem icon={<FaFolderOpen />}> Projects </MenuItem>
-               <MenuItem icon={<FiTarget />}> Targets </MenuItem>
-               <MenuItem icon={<MdMessage />}> Messages </MenuItem>
-               <MenuItem icon={<HiTemplate />}> Templates </MenuItem>
-               <MenuItem icon={<FaHistory />}> History </MenuItem>
-            </Menu>
-        </Sidebar>
-        
+        <div className="background" style={ { display: 'flex', height: '100%' } }>
+            <Sidebar className="sidebar">
+                <Menu>
+                    <MenuItem className="top">NotiManager</MenuItem>
+                </Menu>
+                <Menu>
+                    <MenuItem className="odd" icon={homeIcon} style={ { height: '150px' } }> Home </MenuItem>
+                    <MenuItem className="even" icon={projectIcon} style={ { height: '150px' } }> Projects </MenuItem>
+                    <MenuItem className="odd" icon={targetIcon} style={ { height: '150px' } }> Targets </MenuItem>
+                    <MenuItem className="even" icon={messageIcon} style={ { height: '150px' } }> Messages </MenuItem>
+                    <MenuItem className="odd" icon={templateIcon} style={ { height: '150px' } }> Templates </MenuItem>
+                    <MenuItem className="even" icon={historyIcon} style={ { height: '150px' } }> History </MenuItem>
+                </Menu>
+            </Sidebar>
+            <main>
+                <svg onClick={() => collapseSidebar()}>{toggleIcon}</svg>
+            </main>
+        </div>
+
     )
+        
 }
