5f4db0c6f18fd300be93bc1a9aab4f1c5e2d4c1c
Inseong Hwang <insung151@naver.com>
2022-11-04 13:16:07 +0900

    Project init (#4)
    * Add gitignore
    
    * Create django project
    
    * setting 분리
    
    * Set user model
    
    * DRF setting
    
    * Create react app
    
    * Install material ui
    
    * Install dependencies
    - react-router, axios, redux toolkit
    
    * Add how to run
    
    * Add proxy config
    
    * Add traivs.yml and sonar properties
    
    * Fix front test
    
    * pylint
    
    * Fix sonar
    
    * Fix sonar

---
+++ backend/account/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(email=self.normalize_email(email), username=username)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email,
+            password=password,
+            username=username,
+        )
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = UserManager()
---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
