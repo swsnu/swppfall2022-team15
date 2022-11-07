93dff1ec13fa3990ce7b18d8e01a605407f16849
jaeyoungleeee <jerry96@snu.ac.kr>
2022-11-07 18:37:38 +0900

    add notification status enums

---
+++ frontend/src/Enums.ts
@@ -0,0 +1,8 @@
+export const enum NotificationStatus {
+    // Before Sending
+    PEDNING = "PENDING",
+    // After Sending
+    SUCCESS = "SUCCESS",
+    PARTIAL_SUCCESS = "PARTIAL_SUCCESS",
+    FAILURE = "FAILURE",
+};
