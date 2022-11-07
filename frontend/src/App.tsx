2ec1e5b42cb1e2eadb17474a2857c5fd9c544b2c
jerry96 <35989106+jaeyoungleeee@users.noreply.github.com>
2022-11-07 17:52:20 +0900

    remove obsolete (#18)
    * remove obsolete
    
    * remove obsolete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import NotiSidebar from "./containers/Sidebar/NotiSidebar";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import ProjectDetail from "./components/ProjectDetail/ProjectDetail";
 import ProjectListTable from "./containers/ProjectList";
 
 function App() {
@@ -9,10 +8,8 @@ function App() {
     <BrowserRouter>
       <NotiSidebar/>
       // TODO: Other components should be placed next to sidebar, not below
-      // TODO: Other components should be placed next to sidebar, not below
       <Routes>
         <Route path="/projects/" element={<ProjectListTable/>}/>
-        <Route path="/projects/:id" element={<ProjectDetail/>}/>
       </Routes>
      </BrowserRouter>
   );
