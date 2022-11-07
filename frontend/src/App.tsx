d9e5a999aeccee7e301a2d8ea5e6ee929e6b1806
vietman2 <vietman0@gmail.com>
2022-11-08 04:40:28 +0900

    fix sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,24 @@ import "./App.css";
 import NotiSidebar from "./components/Sidebar/NotiSidebar";
 import ProjectListTable from "./containers/ProjectList/ProjectList";
 import ProjectDetail from "./components/ProjectDetail/ProjectDetail";
+import Home from './containers/Home/Home';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <NotiSidebar/>
-      
-      <Routes>
-        <Route path="/projects/" element={<ProjectListTable/>}/>
-        <Route path="/projects/:id" element={<ProjectDetail/>}/>
-      </Routes>
+      <div className="sidebar">
+        <NotiSidebar />
+      </div>
+      <div className="contents">
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/projects" element={<ProjectListTable />} />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
+        </Routes>
+
+      </div>
+        
      </BrowserRouter>
   );
 }
--- frontend/src/containers/Home/Home.tsx
+++ frontend/src/containers/Home/Home.tsx
@@ -1,5 +1,9 @@
 
 
 export default function Home() {
-    
+    return (
+        <div className="Home">
+            <h1>Home</h1>
+        </div>
+    )
 }
