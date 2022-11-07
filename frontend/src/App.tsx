b401cf72e917474597608aec6c465a9b7cd9512f
jerry96 <35989106+jaeyoungleeee@users.noreply.github.com>
2022-11-07 17:42:18 +0900

    Update frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <NotiSidebar/>
       // TODO: Other components should be placed next to sidebar, not below
+      // TODO: Other components should be placed next to sidebar, not below
       <Routes>
         <Route path="/projects/" element={<ProjectListTable/>}/>
         <Route path="/projects/:id" element={<ProjectDetail/>}/>
