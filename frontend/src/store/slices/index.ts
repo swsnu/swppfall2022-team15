6efb27c75d4eaebcd98831a1d2ba4c5b3a57d299
Git_Hae <55076957+keetae423@users.noreply.github.com>
2022-11-08 21:58:35 +0900

    TargetList UI 추가 (#34)
    * add-target-list
    
    * add-target-list
    
    * fix/add-target-list
    
    * fix/feat-add-target-list

--- frontend/src/store/slices/index.ts
+++ frontend/src/store/slices/index.ts
@@ -1,19 +1,25 @@
-import { combineReducers, PreloadedState, configureStore } from "@reduxjs/toolkit";
+import {
+  combineReducers,
+  PreloadedState,
+  configureStore,
+} from "@reduxjs/toolkit";
 
 import projectReducer from "./project";
 import notificationReducer from "./notifications";
+import targetReducer from "./target";
 import { RootState } from "../index";
 
 const rootReducer = combineReducers({
-    project: projectReducer,
-    notification: notificationReducer,
+  project: projectReducer,
+  notification: notificationReducer,
+  target: targetReducer,
 });
 
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
-    return configureStore({
-        reducer: rootReducer,
-        preloadedState,
-    });
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
 };
 
 export default rootReducer;
---
+++ frontend/src/store/slices/target.ts
@@ -0,0 +1,43 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+import { RootState } from "..";
+import { TargetType } from "../../types";
+
+export const fetchTargets = createAsyncThunk(
+  "target/fetchTargets",
+  async () => {
+    const response = await axios.get<TargetType[]>("/api/target/");
+    return response.data;
+  }
+);
+
+export const fetchTarget = createAsyncThunk(
+  "target/fetchTarget",
+  async (targetId: number) => {
+    const response = await axios.get<TargetType>(`/api/target/${targetId}/`);
+    return response.data;
+  }
+);
+
+// 확인 필요
+const initialState: {
+  targets: TargetType[];
+} = {
+  targets: [],
+};
+
+export const TargetSlice = createSlice({
+  name: "target",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchTargets.fulfilled, (state, action) => {
+      state.targets = action.payload;
+    });
+  },
+});
+
+export const targetListSelector = (state: RootState) => state.target.targets;
+export const targetSelect = (state: RootState) => state.target;
+export default TargetSlice.reducer;
