7ce68fd969dbeb1bb45b9d247f79323b3798e521
jaeyoungleeee <jerry96@snu.ac.kr>
2022-11-08 12:56:16 +0900

    fix

--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 
+from notifications.serializers import NotificationSerializer
+
 
 class NotificationViewSet(ModelViewSet):
     permission_classes = (AllowAny,)
+    serializer_class = NotificationSerializer
