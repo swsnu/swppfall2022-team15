b2f983c5884ff06cf215921a5b584e8713e9076e
keetae423 <55076957+keetae423@users.noreply.github.com>
2022-11-08 21:53:48 +0900

    fix/feat-add-target-list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,30 @@
-
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 
 import "./App.css";
 import NotiSidebar from "./components/Sidebar/NotiSidebar";
 import ProjectListTable from "./containers/ProjectList/ProjectList";
 import ProjectDetail from "./components/ProjectDetail/ProjectDetail";
-import Home from './containers/Home/Home';
-import SignIn from './containers/SignIn/SignIn';
-import SignUp from './containers/SignUp/SignUp';
+import Home from "./containers/Home/Home";
+import SignIn from "./containers/SignIn/SignIn";
+import SignUp from "./containers/SignUp/SignUp";
+import TargetListTable from "./containers/TargetList/TargetList";
 
 const SidebarLayout = () => (
   <>
     <div className="sidebar">
       <NotiSidebar />
     </div>
-    <div className='contents'>
+    <div className="contents">
       <Outlet />
     </div>
   </>
-)
+);
 
 function App() {
   return (
@@ -27,14 +33,13 @@ function App() {
         <Route path="/" element={<Navigate replace to={"/login"} />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-
         <Route element={<SidebarLayout />}>
           <Route path="/home" element={<Home />} />
           <Route path="/projects" element={<ProjectListTable />} />
           <Route path="/projects/:id" element={<ProjectDetail />} />
+          <Route path="/targets" element={<TargetListTable />} />
         </Route>
       </Routes>
-      
     </BrowserRouter>
   );
 }
