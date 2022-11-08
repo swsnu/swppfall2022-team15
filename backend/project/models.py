8bfdfb701dd1c86c5fc12b4fd8dcd2c854dc247a
jaeyoungleeee <jerry96@snu.ac.kr>
2022-11-08 21:40:20 +0900

    update field - add choice

--- backend/project/models.py
+++ backend/project/models.py
@@ -8,9 +8,8 @@ class ProjectTypeChoices(models.TextChoices):
     ORGANIZATION = 'ORGANIZATION'
     INDIVIDUAL = 'INDIVIDUAL'
 
-class Project(TimeStampedModel):
 
+class Project(TimeStampedModel):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    project_type = models.CharField(max_length=20)
-
+    project_type = models.CharField(max_length=20, choices=ProjectTypeChoices.choices)
