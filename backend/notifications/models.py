a3b532b917029abe2fb64b1ac17016aa8a35697d
jaeyoungleeee <jerry96@snu.ac.kr>
2022-11-08 20:47:47 +0900

    update enum class to django text choices

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from enum import Enum
 
 
 # TODO - state definition for notification, notification log, and reservation
-class EnumNotifcationStatus(Enum):
+class EnumNotifcationStatus(models.TextChoices):
     PENDING = 'PENDING'
     SENDING = 'SENDING'
     SUCCESS = 'SUCCESS'
@@ -11,22 +10,22 @@ class EnumNotifcationStatus(Enum):
     FAILURE = 'FAILURE'
 
 
-class EnumNotificationType(Enum):
+class EnumNotificationType(models.TextChoices):
     EMAIL = 'EMAIL'
     SMS = 'SMS'
     API = 'API'
 
 
 class Notification(models.Model):
-    message = models.TextField() # TODO - should be changed to foreign key to message 
+    message = models.TextField()  # TODO - should be changed to foreign key to message
     project = models.ForeignKey('project.Project', on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, choices=[(tag, tag.value) for tag in EnumNotifcationStatus])
-    type = models.CharField(max_length=20, choices=[(tag, tag.value) for tag in EnumNotificationType])
+    status = models.CharField(max_length=20, choices=EnumNotifcationStatus.choices)
+    type = models.CharField(max_length=20, choices=EnumNotificationType.choices)
 
 
 class NotificationLog(models.Model):
     notification = models.ForeignKey('Notification', on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, choices=[(tag, tag.value) for tag in EnumNotifcationStatus])
+    status = models.CharField(max_length=20, choices=EnumNotificationType.choices)
     request = models.JSONField()
     response = models.JSONField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -37,5 +36,5 @@ class NotificationLog(models.Model):
 class Reservation(models.Model):
     notifcation = models.ForeignKey('Notification', on_delete=models.CASCADE)
     reserved_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=[(tag, tag.value) for tag in EnumNotifcationStatus])
+    status = models.CharField(max_length=20, choices=EnumNotifcationStatus.choices)
     # TODO - add target user
