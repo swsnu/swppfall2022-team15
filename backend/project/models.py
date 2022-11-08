2f7151690aecc40a4ec89aa7a0c5c9d2e40a2ca0
jaeyoungleeee <jerry96@snu.ac.kr>
2022-11-08 21:29:06 +0900

    refactor choice value to uppercase

--- backend/project/models.py
+++ backend/project/models.py
@@ -4,11 +4,11 @@ from account.models import User
 from core.models import TimeStampedModel
 from django.db import models
 
+class ProjectTypeChoices(models.TextChoices):
+    ORGANIZATION = 'ORGANIZATION'
+    INDIVIDUAL = 'INDIVIDUAL'
 
 class Project(TimeStampedModel):
-    class ProjectType(str, Enum):
-        ORGANIZATION = 'organization'
-        INDIVIDUAL = 'individual'
 
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
