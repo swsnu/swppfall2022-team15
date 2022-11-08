0fd5da39f0282fd83fd87d2c1f29878d9b0ce7db
insung151 <insung151@naver.com>
2022-11-08 15:43:56 +0900

    Add theme provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ProSidebarProvider } from 'react-pro-sidebar';
-import { Provider } from 'react-redux';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import { Provider } from "react-redux";
+import axios from "axios";
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { store } from './store';
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { store } from "./store";
+import { theme } from "./theme";
+import { ThemeProvider } from "@mui/material/styles";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 axios.defaults.baseURL = "http://localhost:8000/";
-axios.defaults.headers.common['Authorization'] =
-  'Token 303de56231bac7fcd7a54bdac1fa8a2f470c07ef';
-axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common["Authorization"] =
+  "Token 303de56231bac7fcd7a54bdac1fa8a2f470c07ef";
+axios.defaults.headers.common["Content-Type"] = "application/json";
 
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <ProSidebarProvider>
       <Provider store={store}>
         <App />
       </Provider>
     </ProSidebarProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
