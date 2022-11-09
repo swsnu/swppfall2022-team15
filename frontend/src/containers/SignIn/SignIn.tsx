17e412cf85f49bfd71ceeac315cbabfcca96e45c
keetae423 <55076957+keetae423@users.noreply.github.com>
2022-11-09 18:10:10 +0900

    edit signin

--- frontend/src/containers/SignIn/SignIn.tsx
+++ frontend/src/containers/SignIn/SignIn.tsx
@@ -2,36 +2,71 @@ import "./SignIn.css";
 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Stack, TextField } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 
 export default function SignIn() {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    //const [authorized, setAuthorized] = useState<boolean>(false);
-    const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  //const [authorized, setAuthorized] = useState<boolean>(false);
+  const navigate = useNavigate();
 
-    const handleSignIn = async () => {
-        navigate(`/home`);
-    };
-    const handleSignUp = () => {
-        navigate(`/signup`);
-    };
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
 
-    return (
-        <div className="SignIn">
-            <form>
-                <label>
-                    Email
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </label>
-                <br/>
-                <label>
-                    Password
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </label>
-                <br/>
-                <button onClick={() => handleSignIn()}>Sign In</button>
-                <button onClick={() => handleSignUp()}>Sign Up</button>
-            </form>
-        </div>
-    )
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSignIn = async () => {
+    navigate(`/home`);
+  };
+  const handleSignUp = () => {
+    navigate(`/signup`);
+  };
+
+  return (
+    <div className="SignIn">
+      <h2>NotiManager</h2>
+      <form>
+        <Stack spacing={2}>
+          <TextField
+            name="email"
+            label="Email address"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <TextField
+            name="password"
+            label="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </Stack>
+        <br />
+        <Stack spacing={2} direction="row">
+          <LoadingButton
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            onClick={() => handleSignIn()}
+          >
+            Login
+          </LoadingButton>
+          <br />
+          <LoadingButton
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            onClick={() => handleSignUp()}
+          >
+            Sign Up
+          </LoadingButton>
+        </Stack>
+      </form>
+    </div>
+  );
 }
