992b57676994f14e5c78c79115018578793794bb
insung151 <insung151@naver.com>
2022-11-10 19:28:54 +0900

    Fix sidebar testcase

--- frontend/src/components/Sidebar/NotiSidebar.test.tsx
+++ frontend/src/components/Sidebar/NotiSidebar.test.tsx
@@ -2,33 +2,22 @@ import { fireEvent, render, screen } from "@testing-library/react";
 
 import NotiSidebar from "./NotiSidebar";
 import { renderWithProviders } from "../../test-utils/mocks";
+import { MemoryRouter, Route, Routes } from "react-router";
 
-describe('Sidebar Testing', () => {
-    it('should render correctly', () => {
-        renderWithProviders(<NotiSidebar />)
-
-        screen.getByText("Home")
-        screen.getByText("Projects")
-        screen.getByText("Targets")
-        screen.getByText("Messages")
-        screen.getByText("Templates")
-        screen.getByText("History")
-    })
-
-    it('should collapse when icon is clicked', () => {
-        renderWithProviders(<NotiSidebar />)
-
-        const sidebar = screen.getByTestId('sidebar');
-
-        expect(sidebar).toBeInTheDocument();
-
-        const icon = screen.getByTestId("collapseIcon");
-        fireEvent.click(icon);
-
-        
-
-        expect(sidebar).toHaveStyle({
-            width: '80px'
-        })
-    })
-})
+describe("Sidebar Testing", () => {
+  it("should render correctly", () => {
+    renderWithProviders(
+      <MemoryRouter>
+        <Routes>
+          <Route path="" element={<NotiSidebar />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    screen.getByText("Home");
+    screen.getByText("Projects");
+    screen.getByText("Targets");
+    screen.getByText("Messages");
+    screen.getByText("Templates");
+    screen.getByText("History");
+  });
+});
