8fd303e5e9ef7a03566e086568bffe7982fde507
insung151 <insung151@naver.com>
2022-11-13 17:42:07 +0900

    add project list page test

---
+++ frontend/src/containers/ProjectList/ProjectList.test.tsx
@@ -0,0 +1,91 @@
+import { fireEvent, screen, act, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import axios from "axios";
+import { debug } from "console";
+import { EnumProjectType } from "../../Enums";
+import { renderWithProviders } from "../../test-utils/mocks";
+import ProjectList from "./ProjectList";
+
+const mockNavigate = jest.fn();
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  Navigate: (props: any) => {
+    mockNavigate(props.to);
+    return null;
+  },
+  useNavigate: () => mockNavigate,
+}));
+
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
+
+describe("<ProjectList />", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render without errors", () => {
+    renderWithProviders(<ProjectList />, {
+      preloadedState: {
+        project: {
+          projects: [
+            { name: "name", project_type: EnumProjectType.INDIVIDUAL, id: 1 },
+            { name: "name", project_type: EnumProjectType.ORGANIZATION, id: 2 },
+          ],
+          selectedProject: null,
+        },
+      },
+    });
+  });
+
+  it("should handle click icon", async () => {
+    jest.spyOn(axios, "delete").mockImplementation((url: string) => {
+      return Promise.resolve();
+    });
+    renderWithProviders(<ProjectList />, {
+      preloadedState: {
+        project: {
+          projects: [
+            { name: "name", project_type: EnumProjectType.INDIVIDUAL, id: 1 },
+          ],
+          selectedProject: null,
+        },
+      },
+    });
+    const iconButton = screen.getByTestId("icon-button");
+    fireEvent.click(iconButton);
+
+    const deleteButton = screen.getByTestId("delete-button");
+    fireEvent.click(deleteButton);
+  });
+
+  it("should handle click create button", () => {
+    renderWithProviders(<ProjectList />);
+
+    const createButton = screen.getByTestId("create-button");
+    fireEvent.click(createButton);
+
+    userEvent.keyboard("{esc}");
+  });
+
+  it("should handle click row", () => {
+    renderWithProviders(<ProjectList />, {
+      preloadedState: {
+        project: {
+          projects: [
+            { name: "name", project_type: EnumProjectType.INDIVIDUAL, id: 1 },
+          ],
+          selectedProject: null,
+        },
+      },
+    });
+    const cell = screen.getByText("1");
+    fireEvent.click(cell);
+
+    const cell2 = screen.getByText("name");
+    fireEvent.click(cell2);
+  });
+});
