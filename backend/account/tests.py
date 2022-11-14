eb3ac699c5147bfc9d495fc7d99af6492913dc8f
Inseong Hwang <insung151@naver.com>
2022-11-14 21:27:26 +0900

    Add user retrieve api (#87)

--- backend/account/tests.py
+++ backend/account/tests.py
@@ -23,3 +23,14 @@ class SignUpAPITestCase(APITestCase):
             'username': 'test'
         })
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user(self):
+        url = reverse('user')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        user = User.objects.create_user(email='test@test.com', password='test', username='test')
+        self.client.force_login(user)
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(resp.data['username'], user.username)
