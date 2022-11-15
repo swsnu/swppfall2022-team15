c1e4c34ede795899eedff5f76f866c3842debd47
jaeyoungleeee <jerry96@snu.ac.kr>
2022-11-15 19:21:15 +0900

    add service test

---
+++ frontend/src/services/message.test.ts
@@ -0,0 +1,25 @@
+import {createMessage, deleteMessage} from "./message";
+import axios from "axios";
+import {createProject, deleteProject} from "./project";
+
+describe('message', () => {
+    it('should create message - success', () => {
+        jest.spyOn(axios, 'post').mockImplementation(() => Promise.resolve({data: {id: 1}}));
+        createMessage('title', 'content');
+    });
+
+    it('should create message - fail', () => {
+        jest.spyOn(axios, 'post').mockImplementation(() => Promise.reject({response: {data: {id: 1}}}));
+        createMessage('title', 'content');
+    });
+
+    it('should delete message -success', () => {
+        jest.spyOn(axios, 'delete').mockImplementation(() => Promise.resolve({data: {id: 1}}));
+        deleteMessage(1);
+    });
+
+    it('should delete message - failure', () => {
+        jest.spyOn(axios, 'delete').mockImplementation(() => Promise.reject({response: {data: {id: 1}}}));
+        deleteMessage(1)
+    });
+});
