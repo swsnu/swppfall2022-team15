fe91f94549b3feb9a36b7abe557c678f73daecf1
jaeyoungleeee <jerry96@snu.ac.kr>
2022-11-16 14:46:44 +0900

    update MessageCreateModal.test.tsx

--- frontend/src/components/Message/MessageCreateModal.test.tsx
+++ frontend/src/components/Message/MessageCreateModal.test.tsx
@@ -25,4 +25,22 @@ describe("MessageCreateModal", () => {
         const button = getByTestId("create-button");
         fireEvent.click(button);
     });
+
+    it("should handle create button correctly - disable button", () => {
+        const {getByTestId} = renderWithProviders(
+            <MessageCreateModal open={true} handleClose={() => {
+            }}/>,
+            {preloadedState}
+        )
+
+        const projectId = screen.getByTestId("project-id");
+        fireEvent.change(projectId, {target: {value: ""}});
+
+        const contentInput = screen.getByTestId("content-input");
+        fireEvent.change(contentInput, {target: {value: ""}});
+
+        const button = getByTestId("create-button");
+        fireEvent.click(button);
+    });
+
 })
