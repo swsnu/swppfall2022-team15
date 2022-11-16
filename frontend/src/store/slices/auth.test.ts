df0c2dcbe4f1014b36d8789f9bf875460c531f1d
insung151 <insung151@naver.com>
2022-11-16 14:17:25 +0900

    Fix code smell

--- frontend/src/store/slices/auth.test.ts
+++ frontend/src/store/slices/auth.test.ts
@@ -70,36 +70,4 @@ describe("auth reducer", () => {
     expect(store.getState().auth.user).toEqual(null);
     expect(getToken()).toEqual(null);
   });
-
-  // it("should handle fetch message", async () => {
-  //   jest.spyOn(axios, "get").mockImplementation((url: string) => {
-  //     return Promise.resolve({
-  //       data: fakeMessages[0],
-  //     });
-  //   });
-  //   await store.dispatch(fetchMessage(1));
-  //   expect(store.getState().message.selectedMessage).toEqual(fakeMessages[0]);
-  // });
-
-  // it("should handle create message", async () => {
-  //   jest.spyOn(axios, "post").mockResolvedValue({ data: fakeMessages[0] });
-
-  //   await store.dispatch(
-  //     createMessage({
-  //       project: 1,
-  //       content: "test content",
-  //     })
-  //   );
-  //   expect(store.getState().message.selectedMessage).toEqual(fakeMessages[0]);
-  // });
-
-  // it("should handle fetch messages by project id", async () => {
-  //   jest.spyOn(axios, "get").mockImplementation((url: string) => {
-  //     return Promise.resolve({
-  //       data: [fakeMessages[0]],
-  //     });
-  //   });
-  //   await store.dispatch(fetchMessagesByProjectId(1));
-  //   expect(store.getState().message.messages).toEqual([fakeMessages[0]]);
-  // });
 });
