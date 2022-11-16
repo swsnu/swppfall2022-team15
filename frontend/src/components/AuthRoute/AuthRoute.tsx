da39f51acfa9712c38d2c3e0b85bfa46ab3e4a6f
insung151 <insung151@naver.com>
2022-11-16 20:17:16 +0900

    Fix typo

--- frontend/src/components/AuthRoute/AuthRoute.tsx
+++ frontend/src/components/AuthRoute/AuthRoute.tsx
@@ -6,13 +6,12 @@ import { authenticate, authSelector, hasToken } from "../../store/slices/auth";
 export default function AuthRoute({ children }: any) {
   const auth = useSelector(authSelector);
   const dispatch = useDispatch<AppDispatch>();
-
   if (hasToken()) {
     if (!Boolean(auth.user)) {
       dispatch(authenticate());
     }
     return children;
   } else {
-    return <Navigate to={"/signin"} />;
+    return <Navigate to="/login" />;
   }
 }
