ea6994e5cdc89afc78af407e79c29d34c0912cad
insung151 <insung151@naver.com>
2022-11-20 16:06:52 +0900

    Add slack message create api

--- backend/nmessages/serializers.py
+++ backend/nmessages/serializers.py
@@ -7,3 +7,22 @@ class NMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = NMessage
         fields = '__all__'
+
+
+class SlackNMessageSerializer(serializers.ModelSerializer):
+    channel = serializers.CharField(write_only=True)
+    message = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = NMessage
+        fields = ('channel', 'message', 'notification_type', 'content')
+        extra_kwargs = {
+            'content': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        validated_data['content'] = {
+            'channel': validated_data.pop('channel'),
+            'message': validated_data.pop('message')
+        }
+        return super().create(validated_data)
--- backend/nmessages/tests.py
+++ backend/nmessages/tests.py
@@ -2,6 +2,7 @@ from rest_framework.test import APITestCase
 from model_bakery import baker
 
 from account.models import User
+from nmessages.models import NMessage
 from notifications.models import EnumNotificationType
 
 
@@ -14,13 +15,19 @@ class NMessagesAPITestCase(APITestCase):
     def test_create(self):
         # When
         self.client.force_authenticate(user=self.user)
+        data = {'channel': 'channel', 'message': 'message',
+                'notification_type': EnumNotificationType.SLACK}
         response = self.client.post(
             '/api/message/',
-            data={'content': '{}', 'notification_type': EnumNotificationType.HTTP}
+            data=data
         )
 
         # Then
         self.assertEqual(response.status_code, 201)
+        self.assertEqual(
+            NMessage.objects.filter(notification_type=EnumNotificationType.SLACK).last().content[
+                'channel'],
+            data['channel'])
 
     def test_list(self):
         # Given
