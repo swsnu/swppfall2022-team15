50d465e53be6b4a29dfa5590b738c785b1a8f076
jaeyoungleeee <jerry96@snu.ac.kr>
2022-11-20 17:46:21 +0900

    add tasks

--- backend/notifications/services.py
+++ backend/notifications/services.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
 import json
+from datetime import timedelta
 from logging import getLogger
 from typing import TypedDict
 
 import requests
+from django.utils import timezone
 
 from core.exceptions import NotificationServiceException
 from noti_manager.celery import app
+from notifications.models import Notification, EnumNotificationStatus, EnumNotificationType, Reservation, \
+    EnumReservationStatus
 
 logger = getLogger(__name__)
 
@@ -32,3 +36,52 @@ class ApiNotificationDto(TypedDict):
     endpoint: str
     headers: dict
     data: str
+
+
+@app.task
+def task_spawn_notification_by_chunk(reservation_id: int):
+    reservation = Reservation.objects.select_related('notification_group').get(id=reservation_id)
+    notification_ids = reservation.notification_group.notification_set.values_list('id', flat=True)
+
+    def split_notification_ids_by_chunk_size(ids: list[int], chunk_size) -> list[list[int]]:
+        return [
+            ids[offset:offset + chunk_size]
+            for offset in range(0, len(ids), chunk_size)
+        ]
+
+    CHUNK_SIZE = 100
+    notification_ids_by_chunk_size = split_notification_ids_by_chunk_size(notification_ids, CHUNK_SIZE)
+    for notification_ids in notification_ids_by_chunk_size:
+        task_handle_chunk_notification.delay(notification_ids)
+
+@app.task
+def task_handle_chunk_notification(notification_ids: list[int]):
+    """Send a notification to the notification service."""
+    notifications = Notification.objects\
+        .filter(id__in=notification_ids, status=EnumNotificationStatus.PENDING)\
+        .select_related('target_user')
+
+    for notification in notifications:
+        # TODO 하나의 클래스로 추상화 해서 바로 던지는 게 좋을 듯
+        if notification.notification_group.type == EnumNotificationType.HTTP:
+            data = ApiNotificationDto(
+                endpoint=notification.target_user.endpoint,
+                headers=notification.target_user.data,
+                data=notification.request,
+            )
+            task_send_api_notification.delay(data)
+        elif notification.notificaiton_group.type == EnumNotificationType.SMS:
+            pass
+        elif notification.notification_group.type == EnumNotificationType.SLACK:
+            pass
+
+
+@app.task
+def cron_task_handle_reservation():
+    """Handle a reservation."""
+    reservations = Reservation.objects\
+        .filter(status=EnumReservationStatus.PENDING)\
+        .filter(reserved_at__range=(timezone.now(), timezone.now() + timedelta(minutes=1))) # TODO: 1분 이내를 표현할 수 있는 좋은 방식으로 수정할 것
+
+    for reservation in reservations:
+        task_spawn_notification_by_chunk.delay(reservation.id)
