8032ddb9e5f6c2f1d02372749e68ba56b36e6306
insung151 <insung151@naver.com>
2022-11-21 19:34:31 +0900

    Add slack targetuser create api

--- backend/targetusers/tests.py
+++ backend/targetusers/tests.py
@@ -1,7 +1,11 @@
+from django.urls import reverse
 from model_bakery import baker
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 from account.models import User
+from notifications.models import EnumNotificationType
+from targetusers.models import TargetUser
 
 
 class TargetUserAPITestCase(APITestCase):
@@ -13,3 +17,18 @@ class TargetUserAPITestCase(APITestCase):
         self.client.force_authenticate(self.user)
         response = self.client.get('/api/targetuser/')
         self.assertEqual(response.status_code, 200)
+
+    def test_create(self):
+        self.client.force_login(self.user)
+        data = {'notification_type': EnumNotificationType.SLACK,
+                'name': 'name', 'api_key': 'api-key'}
+        response = self.client.post(reverse('targetuser-list'), data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(TargetUser.objects.last().name, data['name'])
+
+    def test_invalid_type_create(self):
+        self.client.force_login(self.user)
+        data = {'notification_type': '.',
+                'name': 'name', 'api_key': 'api-key'}
+        response = self.client.post(reverse('targetuser-list'), data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- backend/targetusers/views.py
+++ backend/targetusers/views.py
@@ -1,7 +1,10 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from notifications.models import EnumNotificationType
 from targetusers.models import TargetUser
-from targetusers.serializers import TargetUserSerializer
+from targetusers.serializers import TargetUserSerializer, SlackTargetUserSerializer
 
 
 class TargetUserViewSet(ModelViewSet):
@@ -12,3 +15,19 @@ class TargetUserViewSet(ModelViewSet):
         if project_id := request.query_params.get('projectId'):
             self.queryset = self.queryset.filter(project_id=int(project_id))
         return super().list(request, *args, **kwargs)
+
+    # pylint: disable=R0801
+    def create(self, request, *args, **kwargs):
+        if not ('notification_type' in request.data and
+                request.data['notification_type'] in EnumNotificationType):
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer_class = self.get_create_serializer_class()
+        serializer = serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # pylint: disable=inconsistent-return-statements
+    def get_create_serializer_class(self):
+        if self.request.data['notification_type'] == EnumNotificationType.SLACK:
+            return SlackTargetUserSerializer
