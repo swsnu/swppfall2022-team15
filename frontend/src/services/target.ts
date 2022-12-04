9b336778e31493e76c393eeea666176d98084141
jerry96 <35989106+jaeyoungleeee@users.noreply.github.com>
2022-12-04 23:34:26 +0900

    Revert "Add target user search component (#126)" (#127)
    This reverts commit 4ed483db72d02fd0047300ecaa83e308d02a16e2.

--- frontend/src/services/target.ts
+++ frontend/src/services/target.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { TargetType } from "../types";
 
 export async function createTarget(
   targetName: string,
@@ -21,17 +20,7 @@ export async function createTarget(
 
 export async function deleteTarget(targetId: number) {
   try {
-    return await axios.delete(`/api/targetuser/${targetId}/`);
-  } catch (e: any) {
-    console.log(e.response);
-  }
-}
-
-export async function fetchTargets(notification_type: string) {
-  try {
-    return await axios.get<TargetType[]>(
-      `/api/targetuser/?notification_type=${notification_type}`
-    );
+    return await axios.delete(`/api/targetuser/${targetId}`);
   } catch (e: any) {
     console.log(e.response);
   }
