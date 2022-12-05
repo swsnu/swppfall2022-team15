0d16748f4a3d27adb02b71a7b1bb98433615ca97
jaeyoungleeee <jerry96@snu.ac.kr>
2022-12-05 16:26:08 +0900

    add name to message
    Signed-off-by: jaeyoungleeee <jerry96@snu.ac.kr>

--- backend/nmessages/models.py
+++ backend/nmessages/models.py
@@ -8,5 +8,6 @@ from notifications.models import EnumNotificationType
 class NMessage(TimeStampedModel):
     """Model definition for NMessages."""
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=True) # TODO shouldn't be nullable
     notification_type = models.CharField(max_length=32, choices=EnumNotificationType.choices)
     content = models.JSONField()
