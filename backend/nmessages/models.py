fa3b4e0c57245f1e602b7949fe23e4f6609b5c7b
jaeyoungleeee <jerry96@snu.ac.kr>
2022-12-05 16:27:44 +0900

    comment
    Signed-off-by: jaeyoungleeee <jerry96@snu.ac.kr>

--- backend/nmessages/models.py
+++ backend/nmessages/models.py
@@ -8,6 +8,6 @@ from notifications.models import EnumNotificationType
 class NMessage(TimeStampedModel):
     """Model definition for NMessages."""
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, null=True) # TODO shouldn't be nullable
+    name = models.CharField(max_length=255, null=True)  # TODO shouldn't be nullable
     notification_type = models.CharField(max_length=32, choices=EnumNotificationType.choices)
     content = models.JSONField()
