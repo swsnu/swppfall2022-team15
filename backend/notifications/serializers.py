45cdabd94ee4c85a0adede6dc1d5bce651477224
jerry96 <35989106+jaeyoungleeee@users.noreply.github.com>
2022-12-07 18:48:14 +0900

    Feat/send reservation (#169)
    * add notification config / reservation request
    
    Signed-off-by: jaeyoungleeee <jerry96@snu.ac.kr>
    
    * xx
    
    Signed-off-by: jaeyoungleeee <jerry96@snu.ac.kr>
    
    Signed-off-by: jaeyoungleeee <jerry96@snu.ac.kr>

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from notifications.models import NotificationConfig, Reservation
 from notifications.services import task_bulk_create_notification
 
+from dateutil.rrule import rrulestr
+
 
 class NotificationConfigSerializer(serializers.ModelSerializer):
     message = serializers.IntegerField(source='nmessage_id')
@@ -15,19 +17,21 @@ class NotificationConfigSerializer(serializers.ModelSerializer):
 class ReservationCreateSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     notification_config = serializers.IntegerField()
-    reserved_at = serializers.DateTimeField()
+    rrule = serializers.CharField()
     target_users = serializers.ListField()
 
     class Meta:
         model = Reservation
-        fields = ('id', 'notification_config', 'reserved_at', 'target_users',)
+        fields = ('id', 'notification_config', 'rrule', 'target_users',)
 
     def create(self, validated_data):
         target_users: list[int] = validated_data.get('target_users')
         notification_config_id = validated_data.get('notification_config')
-        reserved_at = validated_data.get('reserved_at')
+        rrule = validated_data.get('rrule')
 
-        task_bulk_create_notification.delay(reserved_at, target_users, notification_config_id)
+        rrule = rrulestr(rrule)
+        for time in rrule:
+            task_bulk_create_notification.delay(time, target_users, notification_config_id)
 
     def update(self, instance, validated_data):
         pass
