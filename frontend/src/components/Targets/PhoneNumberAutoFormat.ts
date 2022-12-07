7e0659439d5abf2cb90cd6c0cc2df0315f0d89c8
vietman2 <vietman0@gmail.com>
2022-12-07 20:40:27 +0900

    phone number format

--- frontend/src/components/Targets/PhoneNumberAutoFormat.ts
+++ frontend/src/components/Targets/PhoneNumberAutoFormat.ts
@@ -3,5 +3,7 @@ export const phoneNumberAutoFormat = (phoneNumber: string): string => {
 
   if (number.length < 4) return number;
   if (number.length < 7) return `${number.slice(0, 3)}-${number.slice(3)}`;
-  return `${number.slice(0, 3)}-${number.slice(3, 6)}-${number.slice(6, 10)}`;
+  if (number.length < 10) return `${number.slice(0, 3)}-${number.slice(3, 6)}-${number.slice(6, 10)}`;
+  return `${number.slice(0, 3)}-${number.slice(3, 7)}-${number.slice(7, 11)}`;
+
 };
