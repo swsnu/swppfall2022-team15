dcaf67d1b0d48285c022a070ddbd914279132026
jerry96 <35989106+jaeyoungleeee@users.noreply.github.com>
2022-12-07 15:36:56 +0900

    add metrics api (#150)
    Signed-off-by: jaeyoungleeee <jerry96@snu.ac.kr>
    
    Signed-off-by: jaeyoungleeee <jerry96@snu.ac.kr>

--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -1,15 +1,19 @@
+import datetime
+
+from django.db.models import Count
+from django.db.models.functions import Trunc
 from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.decorators import action
 
 from core.permissions import IsOwner
 from nmessages.models import NMessage
+from notifications.models import Notification
 from notifications.models import Reservation
 from notifications.serializers import NotificationConfigSerializer, ReservationCreateSerializer
 from notifications.services import task_send_api_notification, ApiNotificationDto
-from notifications.models import Notification
 from targetusers.models import TargetUser
 
 
@@ -53,6 +57,32 @@ class NotificationViewSet(ModelViewSet):
             notification_config__notification__project__user=request.user
         )
         return Response(data=notifications, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def metrics(self, request):
+        def convert(metric):
+            result = dict()
+            result['status'] = metric['status']
+            result['time'] = datetime.datetime.strftime(metric['key'], '%Y-%m-%d %H:%M:%S')
+            result['count'] = metric['count']
+            return result
+
+        start_time = request.query_params.get('start')
+        end_time = request.query_params.get('end')
+
+        interval = request.query_params.get('interval')
+
+        metrics = Notification.objects.filter(
+            updated_at__range=(start_time, end_time)
+        ).annotate(
+            key=Trunc('updated_at', interval)
+        ).values('status', 'key').annotate(
+            count=Count('key')
+        ).order_by('count', 'status')
+
+        response = list(map(convert, metrics))
+
+        return Response(data=response, status=status.HTTP_200_OK)
 
 
 class ReservationViewSet(ModelViewSet):
