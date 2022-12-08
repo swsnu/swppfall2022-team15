3fdbdfd687f5add77c5725657e62e798407b70af
jaeyoungleeee <jerry96@snu.ac.kr>
2022-12-09 01:17:27 +0900

    fix
    Signed-off-by: jaeyoungleeee <jerry96@snu.ac.kr>

--- frontend/src/containers/MultiStepForm/NotificationTypeStep.tsx
+++ frontend/src/containers/MultiStepForm/NotificationTypeStep.tsx
@@ -9,11 +9,6 @@ interface IProps {
 }
 
 export default function NotificationTypeForm(props: IProps) {
-  const projectState = useSelector(projectSelect);
-
-  if (
-    projectState.selectedProject?.project_type === EnumProjectType.INDIVIDUAL
-  ) {
     return (
       <Select
         fullWidth={true}
@@ -23,22 +18,10 @@ export default function NotificationTypeForm(props: IProps) {
         }
         data-testid="notification-type-select"
       >
+        <MenuItem value={EnumNotificationType.WEBHOOK}>WEBHOOK</MenuItem>
         <MenuItem value={EnumNotificationType.EMAIL}>EMAIL</MenuItem>
         <MenuItem value={EnumNotificationType.SMS}>SMS</MenuItem>
         <MenuItem value={EnumNotificationType.SLACK}>SLACK</MenuItem>
       </Select>
     );
-  } else {
-    return (
-      <Select
-        fullWidth={true}
-        value={props.notificationType}
-        onChange={(event) =>
-          props.setNotificationType(event.target.value as string)
-        }
-      >
-        <MenuItem value={EnumNotificationType.WEBHOOK}>API</MenuItem>
-      </Select>
-    );
-  }
 }
