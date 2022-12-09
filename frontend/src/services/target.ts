c0022339dd816bf0f3b755c63d2c42c681ebad9a
Inseong Hwang <insung151@naver.com>
2022-12-10 01:37:41 +0900

    target update (#190)
    * target update
    
    * Remove unused
    
    * Fix modal title
    
    * Refactor

--- frontend/src/services/target.ts
+++ frontend/src/services/target.ts
@@ -1,9 +1,30 @@
 import axios from "axios";
-
+import { TargetType } from "../types";
 
 export async function deleteTarget(targetId: number) {
   try {
-    return await axios.delete(`/api/targetuser/${targetId}`);
+    return await axios.delete(`/api/targetuser/${targetId}/`);
+  } catch (e: any) {
+    console.log(e.response);
+  }
+}
+
+export async function getTarget(targetId: number) {
+  try {
+    const resp = await axios.get<TargetType>(`/api/targetuser/${targetId}/`);
+    return resp.data;
+  } catch (e: any) {
+    console.log(e.response);
+  }
+}
+
+export async function updateTarget(targetId: number, requestData: any) {
+  try {
+    const resp = await axios.patch<TargetType>(
+      `/api/targetuser/${targetId}/`,
+      requestData
+    );
+    return resp.data;
   } catch (e: any) {
     console.log(e.response);
   }
