0f959f7603b1884ae0730cde9ca1692dc60f5d75
keetae423 <55076957+keetae423@users.noreply.github.com>
2022-12-10 01:22:31 +0900

    fix

--- frontend/src/components/Message/DynamicTable.tsx
+++ frontend/src/components/Message/DynamicTable.tsx
@@ -16,13 +16,13 @@ export default function DynamicTable(props: {
   keys: any;
   rows: any;
   handleOpenMenu: any;
-  onClickRow?: (e:any) => void;
+  onClickRow?: (e: any) => void;
 }) {
   const handleClickRow = (id: number) => {
     if (props.onClickRow) {
       props.onClickRow(id);
     }
-  }
+  };
 
   return (
     <Card>
@@ -39,30 +39,34 @@ export default function DynamicTable(props: {
             <TableBody>
               {props.rows.map((row: any) => {
                 return (
-                  <TableRow hover key={row.id} tabIndex={-1} onClick={()=>handleClickRow(row.id)}>
+                  <TableRow
+                    hover
+                    key={row.id}
+                    tabIndex={-1}
+                    onClick={() => handleClickRow(row.id)}
+                  >
                     {props.keys.map((key: string) => {
-                      const fields = key.split('.')
+                      const fields = key.split(".");
                       let value = row;
 
                       fields.forEach((field) => {
                         value = value[field];
                       });
-                      return (
-                        <TableCell align="left">{value}</TableCell>
-                      );
+                      return <TableCell align="left">{value}</TableCell>;
                     })}
                     {props.handleOpenMenu && (
                       <TableCell align="right">
-                      <IconButton
-                        size="large"
-                        color="inherit"
-                        data-testid="open-menu-button"
-                        onClick={props.handleOpenMenu}
-                        data-id={row.id}
-                      >
-                        <Iconify icon={"eva:more-vertical-fill"} />
-                      </IconButton>
-                    </TableCell>)}
+                        <IconButton
+                          size="large"
+                          color="inherit"
+                          data-testid="open-menu-button"
+                          onClick={props.handleOpenMenu}
+                          data-id={row.id}
+                        >
+                          <Iconify icon={"eva:more-vertical-fill"} />
+                        </IconButton>
+                      </TableCell>
+                    )}
                   </TableRow>
                 );
               })}
