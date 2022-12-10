082f268b454265099067605be7facb7342f920ff
vietman2 <vietman0@gmail.com>
2022-12-11 03:30:37 +0900

    history table

--- frontend/src/containers/History/HistoryTable.tsx
+++ frontend/src/containers/History/HistoryTable.tsx
@@ -5,114 +5,18 @@ import { useDispatch, useSelector } from "react-redux";
 
 import Scrollbar from "../../components/Scrollbar/Scrollbar";
 import HistoryTableHead from "./TableHead";
-import { fetchAllNotifications, notificationListSelector } from "../../store/slices/notifications";
+import { fetchAllNotificationConfigs, notificationConfigList } from "../../store/slices/notificationConfig"
 import { useEffect } from "react";
 import { AppDispatch } from "../../store";
 
 import "./HistoryTable.css"
 
-const fakeData = [
-  {
-    project: "project1",
-    type: "SLACK",
-    endpoint: "endpoint1",
-    reservedAt: "2022-12-10",
-    status: "SUCCESS",
-  },
-  {
-    project: "project2",
-    type: "SLACK",
-    endpoint: "endpoint1",
-    reservedAt: "2022-12-10",
-    status: "SUCCESS",
-  },
-  {
-    project: "project3",
-    type: "SLACK",
-    endpoint: "endpoint1",
-    reservedAt: "2022-12-10",
-    status: "FAILURE",
-  },
-  {
-    project: "project4",
-    type: "SLACK",
-    endpoint: "endpoint1",
-    reservedAt: "2022-12-10",
-    status: "PENDING",
-  },
-  {
-    project: "project1",
-    type: "SMS",
-    endpoint: "010-1234-1234",
-    reservedAt: "2022-12-10",
-    status: "SUCCESS",
-  },
-  {
-    project: "project1",
-    type: "SMS",
-    endpoint: "010-1234-1234",
-    reservedAt: "2022-12-10",
-    status: "FAILURE",
-  },
-  {
-    project: "project1",
-    type: "SMS",
-    endpoint: "010-0000-0000",
-    reservedAt: "2022-12-10",
-    status: "PENDING",
-  },
-  {
-    project: "project2",
-    type: "SMS",
-    endpoint: "010-0000-0000",
-    reservedAt: "2022-12-10",
-    status: "PENDING",
-  },
-  {
-    project: "project2",
-    type: "EMAIL",
-    endpoint: "email@email.com",
-    reservedAt: "2022-12-10",
-    status: "SUCCESS",
-  },
-  {
-    project: "project2",
-    type: "EMAIL",
-    endpoint: "test@test.com",
-    reservedAt: "2022-12-10",
-    status: "SUCCESS",
-  },
-  {
-    project: "project2",
-    type: "EMAIL",
-    endpoint: "user@user.com",
-    reservedAt: "2022-12-10",
-    status: "SUCCESS",
-  },
-  {
-    project: "project3",
-    type: "EMAIL",
-    endpoint: "email@email.com",
-    reservedAt: "2022-12-10",
-    status: "FAILURE",
-  },
-  {
-    project: "project3",
-    type: "WEBHOOK",
-    endpoint: "noti-manager.site",
-    reservedAt: "2022-12-10",
-    status: "PENDING",
-  },
-];
-
-
 export default function HistoryTable() {
-  //const notifications = useSelector(notificationListSelector);
+  const notificationConfigs = useSelector(notificationConfigList);
   const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
-    dispatch(fetchAllNotifications());
+    dispatch(fetchAllNotificationConfigs());
   }, [])
-  const notifications = fakeData;
 
   return (
     <>
@@ -127,22 +31,22 @@ export default function HistoryTable() {
             <Table>
               <HistoryTableHead />
               <TableBody>
-                {notifications.map((notification, index) => (
+                {notificationConfigs.map((notification, index) => (
                   <TableRow key={index}>
                     <TableCell>
-                      <Container>{"Project (Need Fix)"}</Container>
+                      <Container>{notification.project}</Container>
                     </TableCell>
                     <TableCell>
                       <Container>{notification.type}</Container>
                     </TableCell>
                     <TableCell>
-                      <Container>{"Endpoint (Need Fix)"}</Container>
+                      <Container>{"ENDPOINT: NEED FIX"}</Container>
                     </TableCell>
                     <TableCell>
-                      <Container>{notification.reservedAt}</Container>
+                      <Container>{"CREATED AT: NEED FIX"}</Container>
                     </TableCell>
                     <TableCell>
-                      <Container>{notification.status}</Container>
+                      <Container>{"STATUS: NEED FIX"}</Container>
                     </TableCell>
                   </TableRow>
                 ))}
