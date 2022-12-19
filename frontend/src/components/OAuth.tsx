e3971c2e4c583235e6d5096ae92fc4201177b298
jaeyoungleeee <jerry96@snu.ac.kr>
2022-12-19 14:18:25 +0900

    fix oauth
    Signed-off-by: jaeyoungleeee <jerry96@snu.ac.kr>

--- frontend/src/components/OAuth.tsx
+++ frontend/src/components/OAuth.tsx
@@ -1,12 +1,9 @@
-import { useSearchParams } from "react-router-dom";
 import { Navigate } from "react-router";
 import axios from "axios";
 import { useEffect } from "react";
 
 export default function OAuth() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const code = searchParams.get("code");
-  console.log(code);
+  const code = "code";
 
   const sendCode = async () => {
     try {
@@ -15,9 +12,7 @@ export default function OAuth() {
     } catch (error) {}
   };
   useEffect(() => {
-    if (code) {
-      sendCode();
-    }
+    sendCode();
   });
 
   return <Navigate to={"/home"} />;
